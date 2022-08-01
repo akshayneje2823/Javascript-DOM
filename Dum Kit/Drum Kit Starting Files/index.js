f18ed4609bd106e39faf8fb6b30efe57350fd2b1
Akshay Neje <akshayneje2823@gmail.com>
2022-08-01 16:44:24 +0530

    Done

--- Dum Kit/Drum Kit Starting Files/index.js
+++ Dum Kit/Drum Kit Starting Files/index.js
@@ -1,4 +1,12 @@
-document.querySelector('button').addEventListener("click",handleClick)
-function handleClick(){
-    alert("i got clicked")
+var noOfbuttons = document.querySelectorAll(".drum").length
+
+for(var i=0; i<noOfbuttons; i++){
+    document.querySelectorAll('.drum')[i].addEventListener("click",function (){
+        alert("i got clicked")
+    });
+    
+
 }
+
+
+
